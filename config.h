9c85207b1154a56f59a4685eb74936fbdeee5279
lfischer79 <75506047+lfischer79@users.noreply.github.com>
2024-11-24 08:23:17 -0500

    Update config.h

--- config.h
+++ config.h
@@ -1,18 +1,3 @@
-/*
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 #ifndef CONFIG_H
 #define CONFIG_H
@@ -35,12 +20,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_TERM 200
 
-#define ENCODERS_PAD_A { GP12 }
-#define ENCODERS_PAD_B { GP24 }
+#define ENCODERS_PAD_A { GP6 }
 
 #define ENCODER_RESOLUTIONS { 4 }
 
-#define RGB_DI_PIN GP29
+#define RGB_DI_PIN GP37
 #define DRIVER_LED_TOTAL 50
 #define RGB_MATRIX_LED_COUNT 50
 #define RGBLED_NUM 50
